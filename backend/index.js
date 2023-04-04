c4e2a5cb1019348d0341943c5923f530bdf9be18
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-04-04 18:58:39 +0000

    Added multiple endpoints, table styling, few checkboxes that filter data, endpoint filtering etc..

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require('axios');
+
 
 const app = express();
 
@@ -22,7 +24,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-let flights = [
+let arrivals = [
     {
         "Id": 1143537, 
         "No":"FI207",
@@ -84,6 +86,252 @@ let flights = [
         "AltDisplayName":"Mílanó"
     }
 ];
+
+let departures = [
+    {
+        "Id":1143880,
+        "No":"FI562",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"FI",
+        "Airline":"Icelandair",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"FCO",
+        "OriginDest":"Rome",
+        "DisplayName":"Rome",
+        "Gate":"A15",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T08:30:00+00:00",
+        "Estimated":"2023-04-04T10:30:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:15:00+00:00",
+        "AOBT":"2023-04-04T11:08:00+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"TFFIV",
+        "AircraftType":"75W",
+        "ServiceType":"J",
+        "Stand":"5",
+        "Logo":"/media/1/tailfi2.png",
+        "AltDisplayName":null
+    },
+    {
+        "Id":1143889,
+        "No":"SK596",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"SK",
+        "Airline":"SAS",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"CPH",
+        "OriginDest":"Copenhagen",
+        "DisplayName":"Copenhagen",
+        "Gate":"C23",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T10:30:00+00:00",
+        "Estimated":"2023-04-04T11:20:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:49:00+00:00",
+        "AOBT":"2023-04-04T11:34:22+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"OYJYA",
+        "AircraftType":"73H",
+        "ServiceType":"J",
+        "Stand":"9",
+        "Logo":"/media/1/tailsk.png",
+        "AltDisplayName":"Kaupmannahöfn"
+    },
+    {
+        "Id":1143890,
+        "No":"SK4788",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"SK",
+        "Airline":"SAS",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"OSL",
+        "OriginDest":"Oslo",
+        "DisplayName":"Oslo",
+        "Gate":"A11",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T11:05:00+00:00",
+        "Estimated":null,
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:21:00+00:00",
+        "AOBT":"2023-04-04T11:12:26+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"SERUD",
+        "AircraftType":"32N",
+        "ServiceType":"J",
+        "Stand":"1",
+        "Logo":"/media/1/tailsk.png",
+        "AltDisplayName":"Osló"
+    }
+];
+
+let flights = [
+    {
+        "Id": 1143537, 
+        "No":"FI207",
+        "Departure":false,
+        "InAir":false,
+        "AirlineIATA":"FI",
+        "Airline":"Icelandair",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"CPH",
+        "OriginDest":"Copenhagen",
+        "DisplayName":"Copenhagen",
+        "Gate":"C",
+        "BaggageClaim":"3",
+        "Scheduled":"2023-04-03T15:30:00+00:00",
+        "Estimated":"2023-04-03T17:19:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-03T17:19:00+00:00",
+        "AOBT":null,
+        "AIBT":"2023-04-03T17:23:00+00:00",
+        "Status":"Landed",
+        "Additional":"AllBags onBelt",
+        "Codeshare":"B65603",
+        "Aircraft":"TFFIN",
+        "AircraftType":"75W",
+        "ServiceType":"J",
+        "Stand":"78",
+        "Logo":"/media/1/tailfi2.png",
+        "AltDisplayName":"Kaupmannahöfn"
+    },
+    {
+        "Id":1143546,
+        "No":"W65565",
+        "Departure":false,
+        "InAir":false,
+        "AirlineIATA":"W6",
+        "Airline":"WizzAir",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"MXP",
+        "OriginDest":"Milan",
+        "DisplayName":"Milan",
+        "Gate":"C23",
+        "BaggageClaim":"2",
+        "Scheduled":"2023-04-03T17:10:00+00:00",
+        "Estimated":"2023-04-03T18:05:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-03T18:05:00+00:00",
+        "AOBT":null,
+        "AIBT":"2023-04-03T18:10:58+00:00",
+        "Status":"Landed",
+        "Additional":"AllBags onBelt",
+        "Codeshare":"",
+        "Aircraft":"9HWAI",
+        "AircraftType":"32Q",
+        "ServiceType":"J",
+        "Stand":"9",
+        "Logo":"/media/1/tailw6.png",
+        "AltDisplayName":"Mílanó"
+    },
+    {
+        "Id":1143880,
+        "No":"FI562",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"FI",
+        "Airline":"Icelandair",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"FCO",
+        "OriginDest":"Rome",
+        "DisplayName":"Rome",
+        "Gate":"A15",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T08:30:00+00:00",
+        "Estimated":"2023-04-04T10:30:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:15:00+00:00",
+        "AOBT":"2023-04-04T11:08:00+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"TFFIV",
+        "AircraftType":"75W",
+        "ServiceType":"J",
+        "Stand":"5",
+        "Logo":"/media/1/tailfi2.png",
+        "AltDisplayName":null
+    },
+    {
+        "Id":1143889,
+        "No":"SK596",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"SK",
+        "Airline":"SAS",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"CPH",
+        "OriginDest":"Copenhagen",
+        "DisplayName":"Copenhagen",
+        "Gate":"C23",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T10:30:00+00:00",
+        "Estimated":"2023-04-04T11:20:00+00:00",
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:49:00+00:00",
+        "AOBT":"2023-04-04T11:34:22+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"OYJYA",
+        "AircraftType":"73H",
+        "ServiceType":"J",
+        "Stand":"9",
+        "Logo":"/media/1/tailsk.png",
+        "AltDisplayName":"Kaupmannahöfn"
+    },
+    {
+        "Id":1143890,
+        "No":"SK4788",
+        "Departure":true,
+        "InAir":true,
+        "AirlineIATA":"SK",
+        "Airline":"SAS",
+        "HomeAirportIATA":"KEF",
+        "HomeAirport":"KeflavikAirport",
+        "OriginDestIATA":"OSL",
+        "OriginDest":"Oslo",
+        "DisplayName":"Oslo",
+        "Gate":"A11",
+        "BaggageClaim":"",
+        "Scheduled":"2023-04-04T11:05:00+00:00",
+        "Estimated":null,
+        "NextInformation":null,
+        "Actual":"2023-04-04T11:21:00+00:00",
+        "AOBT":"2023-04-04T11:12:26+00:00",
+        "AIBT":null,
+        "Status":"Departed",
+        "Additional":null,
+        "Codeshare":"",
+        "Aircraft":"SERUD",
+        "AircraftType":"32N",
+        "ServiceType":"J",
+        "Stand":"1",
+        "Logo":"/media/1/tailsk.png",
+        "AltDisplayName":"Osló"
+    }
+];
 /* 
     ####################
     #    Endpoints     #
@@ -112,12 +360,58 @@ app.get(apiPath + version + '/flights', (req, res) => {
     return res.status(200).json(returnData);
 });
 
+// filter = {keyProperty: valueProperty}
+// filter[No]=FI&filter[Name]=Finland
+app.get(apiPath + version + '/arrivals/flights', async (req, res) => {
+    try {
+        const response = await axios.get('https://www.isavia.is/fids/arrivals.aspx');
+        
+        const data = response.data.Items;
+        const filter = req.query.filter;
 
-app.get(apiPath + version + '/arrivals/flights', (req, res) => {
-    let returnArrivalData = [];
-    let arrivals = flights.filter(obj => obj.Departure === false);
-    return res.status(200).json(arrivals);
+        if (!filter) {
+            return res.status(200).json(data);
+        } else {
+            const filterArrivals = data.filter(obj => {
+                
+                return Object.keys(filter).every(key => {
+                    return obj.hasOwnProperty(key) && obj[key].toString().includes(filter[key].toString());
+                });
+            });
+            res.status(200).json(filterArrivals);
+        };
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('An error occured when fetching the data.');
+    };
+
 });
+
+
+app.get(apiPath + version + '/departures/flights', async (req, res) => {
+    try {
+        const response = await axios.get('https://www.isavia.is/fids/departures.aspx');
+        
+        const data = response.data.Items;
+        const filter = req.query.filter;
+        
+        if (!filter) {
+            return res.status(200).json(data);
+        } else {
+            const filterDepartures = data.filter(obj => {
+                return Object.keys(filter).every(key => {
+                    return obj.hasOwnProperty(key) && obj[key].toString().includes(filter[key].toString());
+                });
+            });
+            res.status(200).json(filterDepartures);
+        };
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('An error occured when fetching the data.');
+    }
+    
+});
+
 
 // Get the frontpage from index.html
 app.get("/", (req, res) => {
--- frontend/index.js
+++ frontend/index.js
@@ -1,79 +1,122 @@
 // Here we are using constants as this should not be updated
 const API_URL = "http://localhost:3000/api/v1/";
 
-const getFlights = async () => {
-    // GET request for all flights
+const createTable = (obj, tableEl) => {
+    let trEl = document.createElement('tr');
+
+    let flNoEl = document.createElement('td');
+    flNoEl.textContent = obj.No;
+
+    let acTypeEl = document.createElement('td');
+    acTypeEl.textContent = obj.AircraftType;
+
+    let acEl = document.createElement('td');
+    acEl.textContent = obj.Aircraft;
+
+    let originEl = document.createElement('td');
+    originEl.textContent = obj.OriginDest;
+
+    let scheduleEl = document.createElement('td');
+    scheduleEl.textContent = obj.Scheduled.slice(11, 16);
+
+    let estimatedEl = document.createElement('td');
+    if (obj.Estimated === null) {
+        estimatedEl.textContent = "";
+    }
+    else {
+        estimatedEl.textContent = obj.Estimated.slice(11, 16);
+    };
+
+    let statusEl = document.createElement('td');
+    statusEl.textContent = obj.Status;
+
+    let standEl = document.createElement('td');
+    standEl.textContent = obj.Stand;
+
+    let baggageClaimEl = document.createElement('td');
+    baggageClaimEl.textContent = obj.BaggageClaim;
+
+    let gateEl = document.createElement('td');
+    gateEl.textContent = obj.Gate;
+
+    tableEl.appendChild(trEl);
+
+    trEl.appendChild(flNoEl);
+    trEl.appendChild(acTypeEl);
+    trEl.appendChild(acEl);
+    trEl.appendChild(originEl);
+    trEl.appendChild(scheduleEl);
+    trEl.appendChild(estimatedEl);
+    trEl.appendChild(statusEl);
+    trEl.appendChild(standEl);
+    trEl.appendChild(baggageClaimEl);
+    trEl.appendChild(gateEl);
+}
+
+const removeTable = (elements) => {
+    while (elements.hasChildNodes()) {
+        elements.removeChild(elements.firstChild);
+    }
+};
+
+// If no filter is given, we display all flights.
+const getArrivals = async (filterStr = '') => {
     try {
-        const response = await axios(API_URL + 'flights');
-        console.log('Successfully retrived data: ' + response.data);
+        const response = await axios(API_URL + `arrivals/flights?${filterStr}`);
         
-        let dataOutEl = document.getElementById('arrivalData');
+        let arrivalEl = document.getElementById('arrivalData');
         response.data.forEach(obj => {
-            let trEl = document.createElement('tr');
-
-            let flNoEl = document.createElement('td');
-            flNoEl.textContent = obj.No;
-
-            let acTypeEl = document.createElement('td');
-            acTypeEl.textContent = obj.AircraftType;
-
-            let acEl = document.createElement('td');
-            acEl.textContent = obj.Aircraft;
-
-            let originEl = document.createElement('td');
-            originEl.textContent = obj.OriginDest;
-
-            let scheduleEl = document.createElement('td');
-            scheduleEl.textContent = obj.Scheduled.slice(11, 16);
-
-            let estimatedEl = document.createElement('td');
-            estimatedEl.textContent = obj.Estimated.slice(11, 16);
-
-            let statusEl = document.createElement('td');
-            statusEl.textContent = obj.Status;
-
-            let standEl = document.createElement('td');
-            standEl.textContent = obj.Stand;
-
-            let baggageClaimEl = document.createElement('td');
-            baggageClaimEl.textContent = obj.BaggageClaim;
-
-            let gateEl = document.createElement('td');
-            gateEl.textContent = obj.Gate;
-
-            dataOutEl.appendChild(trEl);
-
-            trEl.appendChild(flNoEl);
-            trEl.appendChild(acTypeEl);
-            trEl.appendChild(acEl);
-            trEl.appendChild(originEl);
-            trEl.appendChild(scheduleEl);
-            trEl.appendChild(estimatedEl);
-            trEl.appendChild(statusEl);
-            trEl.appendChild(standEl);
-            trEl.appendChild(baggageClaimEl);
-            trEl.appendChild(gateEl);
+            createTable(obj, arrivalEl);
         });
 
-            /*
-
-                <th>Flight</th> No
-                <th>A/C Type</th> AircraftType
-                <th>A/C Reg</th> Aircraft
-                <th>Origin</th> OriginDest
-                <th>STA</th> Scheduled
-                <th>ETA</th> Estimated
-                <th>Status</th> Status
-                <th>Stand</th> Stand
-                <th>Belt</th> BaggageClaim
-                <th>Gate</th> Gate
-*/
     } 
     catch (error) {
         console.log(error);
     }
     
 };
+getArrivals();
+setInterval(getArrivals, 60 * 1000);
 
+const getDepartures = async (filterStr = '') => {
+    try {
+        const response = await axios(API_URL + `departures/flights?${filterStr}`);
 
-getFlights();
+        let departureEl = document.getElementById('departureData');
+        response.data.forEach(obj => {
+            createTable(obj, departureEl);
+        });
+    }
+    catch (error) {
+        console.log(error);
+    };
+};
+
+getDepartures();
+setInterval(getDepartures, 60 * 1000);
+
+const filterArrival = (filterKey, filterValue, checkbox) => {
+    checkEl = document.getElementById(`${checkbox}`);
+    let arrivalEl = document.getElementById('arrivalData');
+    if (checkEl.checked) {
+        let filterStr = `filter[${filterKey}]=${filterValue}`;
+        removeTable(arrivalEl);
+        getArrivals(filterStr);
+    } else {
+        removeTable(arrivalEl);
+        getArrivals();
+    };
+};
+
+const filterDepartures = (filterKey, filterValue, checkbox) => {
+    checkEl = document.getElementById(`${checkbox}`);
+    let arrivalEl = document.getElementById('departureData');
+    if (checkEl.checked) {
+        let filterStr = `filter[${filterKey}]=${filterValue}`;
+        removeTable(arrivalEl);
+        getDepartures(filterStr);
+    } else {
+        removeTable(arrivalEl);
+        getDepartures();
+    };
+};
