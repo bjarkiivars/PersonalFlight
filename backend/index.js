db0e811710038a250589f4d01ca2ce3ab9f85dee
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-25 20:48:20 +0000

    changes to backend code

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const path = require('path');
 const apiPath = "/api/";
 const version = "v1";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
