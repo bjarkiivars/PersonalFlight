226a95a36d66acea519b919a8dc30f909d75ebfa
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-30 09:53:09 +0000

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,14 @@ app.use(cors({
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  if('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
 });
 
 // Test data
